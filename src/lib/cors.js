4922b185c9d57bb778a33927775086891be21a71
WipobJind <wipobiscoming@gmail.com>
2026-02-21 19:08:38 +0700

    add Dockerfile and update cors

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,6 +1,6 @@
 const corsHeaders = {
   "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Origin": "http://localhost:5173",
+  "Access-Control-Allow-Origin": "https://my-sever-01.southeastasia.cloudapp.azure.com",
   "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Max-Age": "86400",
